c415ca26bcb4c7e28f87793ba53142eee35e2d88
Sardorbek Oqmardiyev <oqmardiyev@gmail.com>
2024-05-04 16:49:29 +0300

    feat: add let

---
+++ index.js
@@ -0,0 +1,27 @@
+let name = prompt("Ismingiz kiriting");
+let surename = prompt("familyangizni kiriting");
+let university = prompt("Oliygoh yoki maktab");
+let nationality = prompt("millatingiz");
+let city = prompt("shahringizni kiriting");
+let age = prompt("yoshingiz");
+let year = prompt("qaysi yilda tug`ilgansiz ?");
+let moon = prompt("qaysi oyda ?");
+console.log(
+  " ismingiz: " +
+    name +
+    " familyangiz:" +
+    surename +
+    " oliygohingiz: " +
+    university +
+    " millatingiz: " +
+    nationality +
+    " tug`ilgan shahringiz: " +
+    city +
+    " yoshigiz:" +
+    age +
+    "da " +
+    " tug`ilgan yilingiz: " +
+    year +
+    " tug`ilgan oyingiz: " +
+    moon
+);
